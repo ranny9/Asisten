c95a67b57f8bdc20a77319ac1b10a838eb8f96e0
ranny9 <116908252+ranny9@users.noreply.github.com>
2026-02-14 02:34:22 +0700

    Update abis.js

--- abis.js
+++ abis.js
@@ -1,11 +1,14 @@
-export const ABIS = {
-  ERC20: [
-    "function balanceOf(address owner) view returns (uint256)",
-    "function approve(address spender, uint256 amount) returns (bool)",
-    "function decimals() view returns (uint8)"
-  ],
-  ROUTER: [
-    "function getAmountsOut(uint amountIn, address[] memory path) view returns (uint[] memory amounts)",
-    "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) returns (uint[] memory amounts)",
-  ]
-};
+export const ERC20_ABI = [
+  "function balanceOf(address) view returns (uint256)",
+  "function approve(address spender, uint256 amount) returns (bool)",
+  "function transfer(address to, uint256 amount) returns (bool)"
+];
+
+export const ROUTER_ABI = [
+  "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) returns (uint[] memory amounts)"
+];
+
+export const WETH_ABI = [
+  "function deposit() payable",
+  "function withdraw(uint wad)"
+];
